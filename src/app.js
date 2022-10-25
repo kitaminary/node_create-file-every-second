50d9bc3421bf3a4762568f9fe986930035a3354a
Yaroslav Kvasnytsia <qwerty300067@gmail.com>
2022-10-25 15:57:32 +0300

    file creator done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const createFileEverySecond = () => {
+  const date = new Date();
+  const timestamp = date.getTime().toString().padStart(2, '0');
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+
+  fs.writeFile(`app-${hours}_${minutes}_${seconds}`, timestamp, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+
+    console.log(
+      `${timestamp}, app-${hours}_${minutes}_${seconds} file was created`
+    );
+  });
+};
+
+setInterval(createFileEverySecond, 1000);
